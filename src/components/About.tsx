b18e55f4774bb315d28c561db5a69cdb8b818b2e
Goldexcool <ogunseitangold105@gmail.com>
2023-11-08 19:38:57 +0100

    push

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 const About = () => {
-    return (
-        <section>
-            <div>
-                <div>
-                    <h1>About <span>Us</span></h1>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default About;
