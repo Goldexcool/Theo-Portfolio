06b4d8a9327502b6ef9868f4873e059525f4f672
Goldexcool <ogunseitangold105@gmail.com>
2023-11-11 02:03:10 +0100

    second commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
 import about from '../../public/WhatsApp Image 2023-10-31 at 23.46.34 (1).jpeg'
@@ -7,36 +8,58 @@ import rocket from '../../public/bx-rocket.svg'
 import groups from '../../public/bx-group.svg'
 import bulb from '../../public/bx-bulb.svg'
 import about3 from '../../public/WhatsApp Image 2023-10-31 at 23.46.34.jpeg'
+import { useState, useEffect } from 'react';
 const About = () => {
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollToTop(window.scrollY > 350);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <main>
-      <section className='flex items-center justify-center px-5 md:px-20 mt-[5rem]'>
+      <section className='flex items-center justify-center px-5 md:px-20  mt-[5rem]'>
         <div>
           <div className='flex justify-center items-center flex-col gap-3 mb-4 sm:mb-[3rem] '>
-            <h2 className='md:text-[50px] text-[30px]'>About <span className='text-main-blue'>Us</span></h2>
-            <hr className='w-[60px] h-[3px] bg-main-blue mb-4' />
-            <h3 className='text-[15px] text-center sm:mb-5 md:text-[20px]'>Our Team: The Architects of Insulation Innovation</h3>
+          <div className='flex justify-center items-center gap-3 mb-[1rem] text-center'>
+            <hr className='w-[100px] h-[3px] bg-main-blue '/>
+            <h2 className='md:text-[50px] text-[30px] text-main-black'>About <span className='text-main-blue'> Us</span></h2>
+            <hr className='w-[100px] h-[3px] bg-main-blue ' />
           </div>
-          <article className=' gap-4 md:gap-[0rem] md:flex-row md:justify-between w-full  flex-col flex '>
-            <div className='flex items-center flex-col md:w-1/2 w-full gap-3 '>
-              <h1 className='md:text-[24px] font-[500] mb-3 text-[17px] md:text-start text-center'>Crafting Excellence in Thermal Insulation: Our Story, Your Solution</h1>
-              <h2 className='md:text-[19px] text-[15px] md:text-left text-center'> At <span className='text-main-blue text-[17px] '>THEO UNITED</span> we are committed to providing exceptional thermal insulation services. Our team brings together over 12 years of individual experience, each member having worked at management levels in various establishments. </h2>
-              <h3 className='mt-3 mb-4 md:text-[19px] text-[15px] md:text-left text-center'>Our collective expertise is now united under a single corporate body to ensure our prospective clients&apos; satisfaction with our services.</h3>
-              <h4 className='md:text-[19px] text-[15px] md:text-left text-center'>
+            <h3 className='text-[15px] text-center sm:mb-5 md:text-[20px] text-main-black'>Our Team: The Architects of Insulation Innovation</h3>
+          </div>
+          <article className=' gap-4 lg:gap-[0rem]  lg:flex-row flex-wrap justify-center items-center  lg:justify-between w-full  flex-col flex text-main-black'>
+            <div className='flex items-center flex-col lg:w-1/2 w-full gap-3 '>
+              <h1 className='md:text-[24px] font-[500] mb-3 text-[17px] lg:text-start text-center'>Crafting Excellence in Thermal Insulation: Our Story, Your Solution</h1>
+              <h2 className='md:text-[19px] text-[15px] lg:text-left text-center'> At <span className='text-main-blue text-[17px] '>THEO UNITED</span> we are committed to providing exceptional thermal insulation services. Our team brings together over 12 years of individual experience, each member having worked at management levels in various establishments. </h2>
+              <h3 className='mt-3 mb-4 md:text-[19px] text-[15px] lg:text-left text-center'>Our collective expertise is now united under a single corporate body to ensure our prospective clients&apos; satisfaction with our services.</h3>
+              <h4 className='md:text-[19px] text-[15px] lg:text-left text-center'>
                 We believe in making a difference through the power of insulation. Together with our clients, we are creating a world that&apos;s not only more energy-efficient but also environmentally responsible. Join us on this journey towards a brighter, better-insulated future.
               </h4>
             </div>
 
-            <div className='md:block flex justify-center items-center'>
-              <Image src={about} alt="" width={500} height={200} className='h-[500px] rounded-[30px] mb-4 w-[500px]' />
+            <div className='lg:block lg:w-fit w-full flex justify-center items-center'>
+              <Image className="w-full" src={about} alt="" width={500} height={200} className='h-[500px] rounded-[30px] mb-4 w-[500px]' />
             </div>
 
-            {/* <div className='flex flex-col gap-3'>
-            <h1 className='text-[27px] text-bold'>Our Mission</h1>
-            <h2 className='text-[17px]'> Our mission is straightforward: to provide our clients with the best thermal insulation services available. We are committed to delivering the highest quality of service to our clients, and we are dedicated to ensuring that our clients are satisfied with our services.
-            </h2>
-            <Image src={about2} alt='' width={700} height={300} className='h-[600px] rounded-[30px]'/>
-          </div> */}
+           
           </article>
         </div>
       </section>
@@ -44,57 +67,70 @@ const About = () => {
 
       <section className='flex items-center justify-center md:px-20 mt-[5rem] px-5'>
         <div className=''>
-          <div className='flex justify-center items-center flex-col gap-3 mb-[3rem] text-center'>
-            <h2 className='md:text-[50px] text-[30px]'>Why Choose <span className='text-main-blue'> THEO UNITED</span></h2>
-            <hr className='w-[60px] h-[3px] bg-main-blue mb-4' />
+          <div className='flex justify-center items-center  gap-[2px] md:gap-3 mb-[3rem] text-center' >
+            <hr className='w-[100px] h-[3px] bg-main-blue '/>
+            <h2 className='md:text-[50px] text-[30px] text-main-black'>Why Choose <span className='text-main-blue'> THEO UNITED</span></h2>
+            <hr className='w-[100px] h-[3px] bg-main-blue ' />
           </div>
 
-          
-          <article className=' flex md:justify-between md:w-full md:flex-row flex-col-reverse gap-4 md:gap-[0px] '>
 
-          <div className='md:block flex justify-center items-center'>
-            <Image src={about3} alt="" width={500} height={200} className='h-[500px] rounded-[30px] mb-4 w-[500px]' />
-          </div>
-          <div className='flex flex-col md:w-1/2 gap-8'>
-            <div className='flex gap-3'>
-              <Image src={bulb} alt='' height={60} width={60} />
-              <div className='flex flex-col gap-2'>
-                <span className='text-[19px]'>Innovation</span>
-                <h1 className='text-[15px]'> Our continuous pursuit of innovation ensures that you receive the latest and most efficient insulation solutions.</h1>
-              </div>
+          <article className=' flex flex-wrap items-center justify-center lg:justify-between lg:w-full lg:flex-row flex-col-reverse gap-4 md:gap-[0px] text-main-black'>
+
+            <div className='lg:block flex justify-center mt-4 lg:mt-0 items-center'>
+              <Image src={about3} className="" alt="" width={500} height={200} className='h-[500px] rounded-[30px] mb-4 w-[500px]' />
             </div>
-
-            <div className='flex gap-3 '>
-              <Image src={checkdouble} alt='' height={60} width={60} />
-              <div className='flex flex-col gap-2'>
-                <span className='text-[20px]'>Quality Assurance</span>
-                <h1 className='text-[15px]'> Our continuous pursuit of innovation ensures that you receive the latest and most efficient insulation solutions.</h1>
+            <div className='flex flex-col lg:w-1/2 gap-8'>
+              <div className='flex gap-3'>
+                <i className='bx bx-bulb text-main-black md:text-[50px] text-[30px]'></i>
+                <div className='flex flex-col gap-2'>
+                  <span className='text-[19px]'>Innovation</span>
+                  <h1 className='text-[15px]'> Our continuous pursuit of innovation ensures that you receive the latest and most efficient insulation solutions.</h1>
+                </div>
               </div>
-            </div>
 
-
-            <div className='flex gap-3 '>
-              <Image src={groups} alt='' height={60} width={60} />
-              <div className='flex flex-col gap-2'>
-                <span className='text-[19px]'>Collaborative Spirit</span>
-                <h1 className='text-[15px]'> Our commitment to quality is unwavering. Every project we undertake is executed with precision and attention to detail, guaranteeing exceptional results.</h1>
+              <div className='flex gap-3 '>
+                <i className='bx bx-check-double text-main-black md:text-[50px] text-[30px]'></i>
+                <div className='flex flex-col gap-2'>
+                  <span className='text-[20px]'>Quality Assurance</span>
+                  <h1 className='text-[15px]'> Our continuous pursuit of innovation ensures that you receive the latest and most efficient insulation solutions.</h1>
+                </div>
               </div>
-            </div>
 
 
-            <div className='flex gap-3  '>
-              <Image src={rocket} alt='' height={70} width={70} />
-              <div className='flex flex-col gap-2'>
-                <span className='text-[19px]'>Experience</span>
-                <h1 className='text-[15px]'>  With over 12 years of experience, our team has honed their skills to perfection. We bring an extensive background in thermal insulation and an understanding of the industry that sets us apart.</h1>
+              <div className='flex gap-3 '>
+                <i className='bx bx-group text-main-black md:text-[50px] text-[30px]'></i>
+                <div className='flex flex-col gap-2'>
+                  <span className='text-[19px]'>Collaborative Spirit</span>
+                  <h1 className='text-[15px]'> Our commitment to quality is unwavering. Every project we undertake is executed with precision and attention to detail, guaranteeing exceptional results.</h1>
+                </div>
               </div>
-            </div>
+
+
+              <div className='flex gap-3  '>
+                <i className='bx bx-rocket text-main-black md:text-[50px] text-[30px]'></i>
+                <div className='flex flex-col gap-2'>
+                  <span className='text-[19px]'>Experience</span>
+                  <h1 className='text-[15px]'>  With over 12 years of experience, our team has honed their skills to perfection. We bring an extensive background in thermal insulation and an understanding of the industry that sets us apart.</h1>
+                </div>
+              </div>
             </div>
           </article>
         </div>
       </section>
 
-  
+
+     ``
+
+      
+      {showScrollToTop && (
+        <div
+          className="fixed bottom-8 right-8 bg-main-blue p-3 rounded-[10px] cursor-pointer"
+          onClick={scrollToTop}
+        >
+          <i className="bx bxs-up-arrow-alt text-white text-3xl"></i>
+        </div>)}
+
+
     </main>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,86 @@ const Hero = () => {
   const [isOpen, setIsOpen] = useState(false);
   const themeButton = useRef();
 
+  const headerRef = useRef(null);
+  const [scrolled, setScrolled] = useState(false);
+
+  // Function to handle the scroll event
+  const handleScroll = () => {
+    // Get the current scroll position
+    const scrollPosition = window.scrollY;
+
+    // Update the state based on the scroll position
+    setScrolled(scrollPosition >= 50);
+  };
+
+  // Attach the handleScroll function to the scroll event using useEffect
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  const isLocalStorageAvailable =
+    typeof window !== "undefined" && window.localStorage;
+  const [selectedTheme, setSelectedTheme] = useState(
+    isLocalStorageAvailable
+      ? localStorage.getItem("selected-theme") || "light"
+      : null
+  );
+  const [selectedIcon, setSelectedIcon] = useState(
+    isLocalStorageAvailable
+      ? localStorage.getItem("selected-icon") || "bx-sun"
+      : null
+  );
+
+  const darkTheme = "dark-theme";
+  const iconTheme = "bx-sun"; // Without specific icon class
+
+  const getCurrentTheme = () => (selectedTheme === "dark" ? "dark" : "light");
+  const getCurrentIcon = () =>
+    selectedIcon === "bx bx-moon" ? "bx bx-moon" : "bx bx-sun";
+
+  const handleButtonClick = () => {
+    setSelectedTheme((currentTheme) =>
+      currentTheme === "dark" ? "light" : "dark"
+    );
+    setSelectedIcon((currentIcon) =>
+      currentIcon === "bx bx-moon" ? "bx bx-sun" : "bx bx-moon"
+    );
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Check the user's system preference for dark mode
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        setSelectedTheme("dark");
+      } else {
+        setSelectedTheme("light");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Apply the selected theme and icon to the body and themeButton
+      document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+      );
+      themeButton.current.classList[
+        selectedIcon === "bx bx-moon" ? "add" : "remove"
+      ](iconTheme);
+
+      // Store the selected theme and icon in local storage
+      localStorage.setItem("selected-theme", getCurrentTheme());
+      localStorage.setItem("selected-icon", getCurrentIcon());
+    }
+  }, [selectedTheme, selectedIcon, darkTheme, iconTheme]);
+
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
@@ -17,25 +97,29 @@ const Hero = () => {
   };
   return (
     <main className='relative bg-hero-pattern px-4'>
-      <header className='flex items-center justify-center max-w-full position-fixed  '>
-        <nav className='flex items-center w-full justify-between px-[0rem]  md:[6em]'>
+      <header
+        ref={headerRef}
+        className={`flex items-center header justify-center max-w-full w-full p-3 px-[1rem] md:px-[4rem] ${scrolled ? 'scrolled' : ''
+          }`}
+      >
+        <nav className='flex items-center w-full justify-between px-[0rem]  '>
           <h1 className='text-[25px] text-white'>THEO UNITED</h1>
           {/* <Image src={logo} alt="logo" width={100} height={10}  /> */}
-          <div className="flex items-center justify-center gap-4 text-main-gray-light text-[20px] hidden md:inline-flex"  >
+          <div className="flex items-center justify-center gap-4 text-[20px] hidden lg:inline-flex text-[#fff]"  >
             <a href="/">Home</a>
             <a href="/create">About Us</a>
             <a href="/create">Services</a>
             <a href="/create">Gallery</a>
             <a href="/create">Contact Us</a>
           </div>
-          <div className='flex items-center justify-center gap-2 cursor-pointer text-[17px] hidden md:inline-flex text-main-gray-light '>
-            <i className='bx bx-phone text-main-gray-light  text-[20px]'></i>
-            <h1 className='text--main-black'>08070583707</h1>
+          <div className='flex items-center justify-center gap-2 cursor-pointer text-[17px] hidden lg:inline-flex text-[#fff]'>
+            <i className='bx bx-phone  text-[20px] text-[#fff]'></i>
+            <h1 className='text-[#fff]'>08070583707</h1>
           </div>
           <div className='flex justify-center items-center gap-3'>
-            <i className='bx bx-moon text-main-gray-light  text-[20px] cursor-pointer ' ></i>
-            <div className='md:hidden'>
-              <i className='bx bx-grid-alt text-[20px] cursor-pointer blc md:hidden text-main-gray-light ' onClick={toggleMenu}></i>
+            <i className='bx bx-moon text-[#fff] text-[20px] cursor-pointer ' onClick={handleButtonClick} ref={themeButton} ></i>
+            <div className='lg:hidden'>
+              <i className='bx bx-grid-alt text-[#fff] text-[20px] cursor-pointer blc md:hidden' onClick={toggleMenu}></i>
             </div>
           </div>
         </nav>
@@ -58,7 +142,7 @@ const Hero = () => {
 
       <section className='w-full flex justify-center h-[100vh]'>
         <div className='flex items-center flex-col justify-center h-screen gap-3 px-[0rem] md:px-[6rem]'>
-          <p className='md:text-[30px] text-white text-center text-[20px]' >
+          <p className='md:text-[30px] text-white text-center text-[20px] ' >
             <span className='text-[25px]  md:text-[45px]'>Thoe United</span><br /> with over 12 years of expertise, offers tailored thermal insulation solutions that exceed industry standards, ensuring excellence in every project.
           </p>
           <button className='px-[20px] py-[10px] md:text-[20px] text-[17px] bg-main-blue text-white rounded-[10px]'>Contact Us</button>
